39fa0ae76ea62c71f2e9bfc409484667293dff8e
thiagosz63 <thiagosz63@gmail.com>
2024-10-09 17:02:20 -0300

    'Family of 3' filter

--- src/main/java/filtros/Filtro.java
+++ src/main/java/filtros/Filtro.java
@@ -5,50 +5,65 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 public class Filtro {
-	public static List<String> tiraSequenciaSeguida(List<String>list){
-		List<String> saida = new ArrayList<String>();
-		
-		// remove numeros seguidos 
-		for (int i = 0; i < list.size(); i++) {
-			String[] vet = list.get(i).split(Pattern.quote("-")); // lendo_o_campo_0_do_txt
+	public static List<String> tiraSequenciaSeguida(List<String> listInput) {
+		List<String> listOutput = new ArrayList<String>();
+
+		// remove numeros seguidos
+		for (String input : listInput) {
+			String[] vet = input.split(Pattern.quote("-"));
 
 			boolean controledeSaida = true;
 			for (int j = 0; j < vet.length; j++) {
 				Integer num = Integer.parseInt(vet[j]);
-				
+
 				for (int k = 0; k < vet.length - 1; k++) {
 					Integer numComparar = Integer.parseInt(vet[k + 1]);
-					if((num + 1) == numComparar) {
+					if ((num + 1) == numComparar) {
 						controledeSaida = false;
 						break;
 					}
 				}
-				
-				if(controledeSaida == false) {
+
+				if (controledeSaida == false) {
 					break;
 				}
 			}
-			if(controledeSaida == true) {
-				saida.add(list.get(i));
+			if (controledeSaida == true) {
+				listOutput.add(input);
 			}
 		}
-		return saida;
+		return listOutput;
 	}
-	
-	public static List<String> tiraSequenciaFamilia(List<String>listInput){
-		List<String> saida = new ArrayList<String>();
-		
+
+	public static List<String> tiraSequenciaFamilia(List<String> listInput) {
+		List<String> listOutput = new ArrayList<String>();
+		boolean resultado = false;
+
 		for (String input : listInput) {
 			String[] vet = input.split(Pattern.quote("-"));
-						
+
 			for (int j = 0; j < vet.length; j++) {
-				String posicao1Vetor = vet[j].substring(0,1);
-				
-				for (int k = 0; k < vet.length - 1; k++) {
-					String posicao2Vetor = vet[j+1].substring(0,1);
+				String posicao1Vetor = vet[j].substring(0, 1);
+
+				for (int k = 0; k < vet.length - 1 - j; k++) {
+					String posicao2Vetor = vet[k + 1 + j].substring(0, 1);
+
+					if (posicao1Vetor.equals(posicao2Vetor)) {
+
+						for (int i = 0; i < vet.length - 2 - k - j; i++) {
+							String posicao3Vetor = vet[i + 2 + k + j].substring(0, 1);
+
+							if (posicao2Vetor.equals(posicao3Vetor)) {
+								resultado = true;
+							}
+						}
+					}
 				}
 			}
+			if (resultado == false) {
+				listOutput.add(input);
+			}
 		}
-		return saida;
+		return listOutput;
 	}
 }
